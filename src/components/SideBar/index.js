6f7dd18bd9d3164ea29dc55816f8874f1ee30d02
Nikola Anastasov Velichkov <federlizer@gmail.com>
2019-09-07 14:22:03 +0200

    Implement side bar navigation

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -2,8 +2,20 @@ import React from 'react'
 import './styles.css'
 
 function SideBar(props) {
+  // page format { name: string, repr: string }
+  const pages = props.pages.map((page, i) => {
+    return (
+      <div key={i} onClick={() => props.handlePageChange(page.name)}>
+        {page.repr}
+      </div>
+    )
+  })
+
   return (
-    <div>Side bar</div>
+    <div className="SideBar">
+      <div><h2>Logo</h2></div>
+      {pages}
+    </div>
   )
 }
 
--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -3,13 +3,37 @@ import React from 'react'
 import './styles.css'
 
 import SideBar from '../../components/SideBar'
+import QuestionsPage from '../QuestionsPage'
 
-function App(props) {
-  return (
-    <div>
-      <SideBar />
-    </div>
-  )
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.changePage = this.changePage.bind(this)
+  }
+
+  changePage(newPage) {
+    alert('Not yet implemented')
+  }
+
+  render() {
+    // page format { name: string, repr: string }
+    const pages = [
+      { name: 'QUESTIONS_PAGE', repr: 'Въпроси'},
+      { name: 'TESTS_PAGE', repr: 'Тестове'},
+    ]
+
+    return (
+      <div className="App">
+        <div className="side">
+          <SideBar pages={pages} handlePageChange={this.changePage} />
+        </div>
+        <div className="page">
+          <QuestionsPage />
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App
