d42b0a6ed9a6d13bcff6c7b21df2a60ee00aafca
Nikola Anastasov Velichkov <federlizer@gmail.com>
2019-10-17 15:45:03 +0200

    create config files

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  resolve: {
+    extensions: ['.jsx', '.js'],
+    mainFiles: ['index'],
+  },
+  entry: {
+    landing: path.resolve(__dirname, 'src/pages/landing.jsx'),
+    teacher: path.resolve(__dirname, 'src/pages/teacher.jsx'),
+    student: path.resolve(__dirname, 'src/pages/student.jsx'),
+    admin: path.resolve(__dirname, 'src/pages/admin.jsx'),
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|.git|build)/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+
+      {
+        test: /\.(css)$/,
+        exclude: /(node_modules|.git|build)/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      chunks: ['landing'],
+      title: 'Log In',
+      filename: 'landing.html',
+      template: path.resolve(__dirname, 'templates/basic.html'),
+    }),
+    new HtmlWebpackPlugin({
+      chunks: ['teacher'],
+      title: 'Teachers',
+      filename: 'teacher.html',
+      template: path.resolve(__dirname, 'templates/basic.html'),
+    }),
+    new HtmlWebpackPlugin({
+      chunks: ['student'],
+      title: 'Students',
+      filename: 'student.html',
+      template: path.resolve(__dirname, 'templates/basic.html'),
+    }),
+    new HtmlWebpackPlugin({
+      chunks: ['admin'],
+      title: 'Admin Panel',
+      filename: 'admin.html',
+      template: path.resolve(__dirname, 'templates/basic.html'),
+    }),
+  ],
+};
