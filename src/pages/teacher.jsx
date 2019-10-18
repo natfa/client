5f4cf4a1d064e54b4d02c85a0e462dfd05d8f1e6
Nikola Anastasov Velichkov <federlizer@gmail.com>
2019-10-18 14:44:06 +0200

    update page's main component

--- src/pages/teacher.jsx
+++ src/pages/teacher.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Component = <h1>Hello teacher.jsx!</h1>;
+const Component = () => (<h1>Hello teacher.jsx!</h1>);
 
 ReactDOM.render(<Component />, document.getElementById('root'));
