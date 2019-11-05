0c7fa556f306571b885d3dac112bbba2a5da26e9
Nikola Anastasov Velichkov <federlizer@gmail.com>
2019-11-05 12:44:56 +0100

    implement test creation sidebar

---
+++ src/components/test-creation-sidebar/index.jsx
@@ -0,0 +1,99 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Grid from '@material-ui/core/Grid';
+import Divider from '@material-ui/core/Divider';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+
+import {
+  DateTimePicker,
+  KeyboardTimePicker,
+} from '@material-ui/pickers';
+
+import withDateUtils from '../../utils/withDateUtils';
+
+const TestCreationSidebar = ({
+  name,
+  onNameChange,
+
+  date,
+  onDateChange,
+
+  timeToSolve,
+  onTimeToSolveChange,
+
+  totalQuestionCount,
+  totalPoints,
+}) => (
+  <Grid container direction="column" spacing={3}>
+    <Grid item>
+      <TextField
+        value={name}
+        onChange={(e) => onNameChange(e.target.value)}
+        margin="dense"
+        fullWidth
+        variant="outlined"
+        label="Име"
+      />
+    </Grid>
+
+    <Grid item>
+      <KeyboardTimePicker
+        value={timeToSolve}
+        onChange={onTimeToSolveChange}
+        ampm={false}
+        inputVariant="outlined"
+
+        margin="dense"
+        fullWidth
+        label="Време за решаване"
+      />
+    </Grid>
+
+    <Grid item>
+      <DateTimePicker
+        format="MMM D YYYY, HH:mm"
+        ampm={false}
+        value={date}
+        onChange={onDateChange}
+        variant="inline"
+        inputVariant="outlined"
+
+        margin="dense"
+        fullWidth
+        label="Дата и час на започване"
+      />
+    </Grid>
+
+    <Grid item>
+      <Divider />
+    </Grid>
+
+    <Grid item>
+      <Typography>{`Общи точки: ${totalPoints}`}</Typography>
+    </Grid>
+
+    <Grid item>
+      <Typography>{`Общ брой въпроси: ${totalQuestionCount}`}</Typography>
+    </Grid>
+  </Grid>
+);
+
+TestCreationSidebar.propTypes = {
+  name: PropTypes.string.isRequired,
+  onNameChange: PropTypes.func.isRequired,
+
+  // Date-ing is hard...
+  date: PropTypes.any.isRequired, // eslint-disable-line react/forbid-prop-types
+  onDateChange: PropTypes.func.isRequired,
+
+  // Date-ing is hard...
+  timeToSolve: PropTypes.any.isRequired, // eslint-disable-line react/forbid-prop-types
+  onTimeToSolveChange: PropTypes.func.isRequired,
+
+  totalQuestionCount: PropTypes.number.isRequired,
+  totalPoints: PropTypes.number.isRequired,
+};
+
+export default withDateUtils(TestCreationSidebar);
--- src/containers/test-creator/index.jsx
+++ src/containers/test-creator/index.jsx
@@ -1,20 +1,48 @@
 import React from 'react';
+import Dayjs from '@date-io/dayjs';
 
 import Grid from '@material-ui/core/Grid';
 
 import TestCreationFilters from '../test-creation-filters';
+import TestCreationSidebar from '../../components/test-creation-sidebar';
+
+import { pointValues } from '../../constants';
 
 class TestCreator extends React.Component {
   constructor(props) {
     super(props);
 
+    const dayjs = new Dayjs();
+    const now = new Date(Date.now());
+    now.setHours(0, 0, 0);
+
     this.state = {
+      name: '',
+      date: dayjs.date(),
+      timeToSolve: dayjs.date(now),
       filters: [],
     };
+
+
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleDateChange = this.handleDateChange.bind(this);
+    this.handleTimeToSolveChange = this.handleTimeToSolveChange.bind(this);
 
     this.handleFilterInsert = this.handleFilterInsert.bind(this);
     this.handleFilterUpdate = this.handleFilterUpdate.bind(this);
     this.handleFilterDelete = this.handleFilterDelete.bind(this);
+  }
+
+  handleNameChange(name) {
+    this.setState((state) => ({ ...state, name }));
+  }
+
+  handleDateChange(date) {
+    this.setState((state) => ({ ...state, date }));
+  }
+
+  handleTimeToSolveChange(timeToSolve) {
+    this.setState((state) => ({ ...state, timeToSolve }));
   }
 
   handleFilterInsert(subject) {
@@ -60,14 +88,46 @@ class TestCreator extends React.Component {
   }
 
   render() {
-    const { filters } = this.state;
+    const {
+      name,
+      date,
+      timeToSolve,
+      filters,
+    } = this.state;
+
+    let totalPoints = 0;
+
+    // IMPORTANT
+    // this operation has a side effect:
+    // the total points are being counted as the total question count is being figured out
+    // this is done so that there aren't two separate computations that would go through the same
+    // operations. I did this because on my end applying filters was being very slow
+    const totalQuestionCount = filters.reduce((tqcAcc, filter) => {
+      const filterQuestionCount = filter.themeFilters.reduce((fqcAcc, themeFilter) => {
+        const themeQuestionCount = pointValues.reduce((thqcAcc, pointValue) => {
+          totalPoints += pointValue * themeFilter[pointValue];
+          return thqcAcc + themeFilter[pointValue];
+        }, 0);
+
+        return fqcAcc + themeQuestionCount;
+      }, 0);
+
+      return tqcAcc + filterQuestionCount;
+    }, 0);
 
     return (
       <Grid spacing={2} container direction="row-reverse">
         <Grid item xs={12} sm={3}>
-          <div>
-            <p>Sidebar</p>
-          </div>
+          <TestCreationSidebar
+            name={name}
+            onNameChange={this.handleNameChange}
+            date={date}
+            onDateChange={this.handleDateChange}
+            timeToSolve={timeToSolve}
+            onTimeToSolveChange={this.handleTimeToSolveChange}
+            totalQuestionCount={totalQuestionCount}
+            totalPoints={totalPoints}
+          />
         </Grid>
 
         <Grid item xs={12} sm={9}>
