235f2a2af8e85b18f2c51799add3a36ea62f6dbd
Nikola Anastasov Velichkov <federlizer@gmail.com>
2019-11-19 12:11:23 +0100

    added sidebar content

--- src/containers/exam-view/ExamView.jsx
+++ src/containers/exam-view/ExamView.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import dayjs from 'dayjs';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 
@@ -41,10 +42,29 @@ class ExamView extends React.Component {
       return <LoadingAnimation />;
     }
 
+    const start = dayjs(exam.startDate).format('DD MMM YYYY, HH:mm');
+
+
+    let timeToSolve = `${exam.timeToSolve.hours < 10
+      ? `0${exam.timeToSolve.hours}`
+      : exam.timeToSolve.hours}:`;
+
+    timeToSolve += `${exam.timeToSolve.minutes < 10
+      ? `0${exam.timeToSolve.minutes}`
+      : exam.timeToSolve.minutes}`;
+
     return (
       <Grid spacing={2} container direction="row-reverse" style={{ height: '100%' }}>
-        <Grid item xs={12} sm={3}>
-          Sidebar
+        <Grid container direction="column" item xs={12} sm={3}>
+          <Grid container direction="row" justify="space-between" item>
+            <Typography color="primary">Начало:</Typography>
+            <Typography>{start}</Typography>
+          </Grid>
+
+          <Grid container direction="row" justify="space-between" item>
+            <Typography color="primary">Продължителност:</Typography>
+            <Typography>{timeToSolve}</Typography>
+          </Grid>
         </Grid>
 
         <Grid item xs={12} sm={9} style={{ height: '100%', overflow: 'auto', overflowX: 'hidden' }}>
