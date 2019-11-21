280254bb3a4897d5a1574d6aa1ecec75a2cc9635
Nikola Anastasov Velichkov <federlizer@gmail.com>
2019-11-21 12:57:19 +0100

    implement media viewing

--- src/components/exam-question-item/ExamQuestionItem.jsx
+++ src/components/exam-question-item/ExamQuestionItem.jsx
@@ -4,89 +4,125 @@ import PropTypes from 'prop-types';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
 
+import MediaList from '../media-list';
+import MediaListItem from '../media-list-item';
 import PaddedPaper from '../padded-paper';
 
-const answers = [
-  {
-    id: '1',
-    text: 'Exampple correct 1',
-    correct: true,
-  },
-  {
-    id: '2',
-    text: 'A very long incorrect answer that hopefully is going to wrap around and do stuff',
-    correct: false,
-  },
-  {
-    id: '3',
-    text: 'Another relatively long answer blah blah lorem ipsum fuck you lala',
-    correct: false,
-  },
-  {
-    id: '4',
-    text: 'Example incorrect 3',
-    correct: false,
-  },
-];
+class ExamQuestionItem extends React.Component {
+  constructor(props) {
+    super(props);
 
-const ExamQuestionItem = ({ question }) => (
-  <PaddedPaper square elevation={2}>
-    <Grid
-      container
-      direction="column"
-      justify="space-between"
-      wrap="nowrap"
-    >
-      <Grid item>
-        <Typography>{question.text}</Typography>
-      </Grid>
+    this.state = {
+      dialogOpen: false,
+    };
 
-      <Grid item>
-        <ul>
-          <Grid
-            container
-            direction="row"
-            justify="space-between"
-            wrap="wrap"
-            spacing={2}
-          >
-            {answers.map((answer) => {
-              const props = answer.correct
-                ? { style: { color: 'green' } }
-                : { color: 'error' };
+    this.openDialog = this.openDialog.bind(this);
+    this.closeDialog = this.closeDialog.bind(this);
+  }
 
-              return (
-                <Grid key={answer.id} item xs={6}>
-                  <li>
-                    <Typography {...props}>
-                      {answer.text}
-                    </Typography>
-                  </li>
-                </Grid>
-              );
-            })}
+  openDialog() {
+    this.setState((state) => ({ ...state, dialogOpen: true }));
+  }
+
+  closeDialog() {
+    this.setState((state) => ({ ...state, dialogOpen: false }));
+  }
+
+  render() {
+    const { dialogOpen } = this.state;
+    const { question } = this.props;
+
+    return (
+      <PaddedPaper square elevation={2}>
+        <Grid
+          container
+          direction="column"
+          justify="space-between"
+          wrap="nowrap"
+        >
+          <Grid item>
+            <Typography>{question.text}</Typography>
           </Grid>
-        </ul>
-      </Grid>
 
-      <Grid
-        item
-        container
-        justify="space-between"
-        alignItems="center"
-      >
-        <span>{`Точки: ${question.points}`}</span>
-        <Button variant="text">прикачени файлове</Button>
-      </Grid>
-    </Grid>
-  </PaddedPaper>
-);
+          <Grid item>
+            <ul>
+              <Grid
+                container
+                direction="row"
+                justify="space-between"
+                wrap="wrap"
+                spacing={2}
+              >
+                {question.answers.map((answer) => {
+                  const props = answer.correct
+                    ? { style: { color: 'green' } }
+                    : { color: 'error' };
+
+                  return (
+                    <Grid key={answer.id} item xs={6}>
+                      <li>
+                        <Typography {...props}>
+                          {answer.text}
+                        </Typography>
+                      </li>
+                    </Grid>
+                  );
+                })}
+              </Grid>
+            </ul>
+          </Grid>
+
+          <Grid
+            item
+            container
+            justify="space-between"
+            alignItems="center"
+          >
+            <span>{`Точки: ${question.points}`}</span>
+            <Button
+              onClick={this.openDialog}
+              disabled={!question.media}
+              variant="text"
+            >
+              прикачени файлове
+            </Button>
+          </Grid>
+        </Grid>
+
+        {question.media
+        && (
+          <Dialog
+            open={dialogOpen}
+            onClose={this.closeDialog}
+          >
+            <DialogTitle align="center">Прикачени файлове</DialogTitle>
+            <DialogContent dividers>
+              <MediaList>
+                {question.media.map((media) => (
+                  <MediaListItem
+                    key={media.url}
+                    src={media.url}
+                  />
+                ))}
+              </MediaList>
+            </DialogContent>
+          </Dialog>
+        )}
+      </PaddedPaper>
+    );
+  }
+}
 
 ExamQuestionItem.propTypes = {
   question: PropTypes.shape({
     text: PropTypes.string,
     points: PropTypes.number,
+    answers: PropTypes.arrayOf(PropTypes.object),
+    media: PropTypes.arrayOf(PropTypes.object),
   }).isRequired,
 };
 
