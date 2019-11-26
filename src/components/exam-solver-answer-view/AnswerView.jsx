59f92626a527f490d3be3f005e87628e2b9122ec
Nikola Anastasov Velichkov <federlizer@gmail.com>
2019-11-26 13:11:14 +0100

    fully implement exam solver question view

--- src/components/exam-solver-answer-view/AnswerView.jsx
+++ src/components/exam-solver-answer-view/AnswerView.jsx
@@ -1,17 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import Radio from '@material-ui/core/Radio';
+
 const AnswerView = ({
   answers,
-}) => answers.map((answer) => (
-  <p key={answer.id}>{answer.text}</p>
-));
+  selectedAnswerId,
+
+  onAnswerChange,
+}) => (
+  <RadioGroup value={selectedAnswerId} onChange={onAnswerChange}>
+    {answers.map((answer) => (
+      <FormControlLabel
+        key={answer.id}
+        value={answer.id}
+        label={answer.text}
+        labelPlacement="end"
+        control={<Radio color="primary" />}
+      />
+    ))}
+  </RadioGroup>
+);
 
 AnswerView.propTypes = {
   answers: PropTypes.arrayOf(PropTypes.shape({
     id: PropTypes.string,
     text: PropTypes.string,
   })).isRequired,
+  selectedAnswerId: PropTypes.string,
+
+  onAnswerChange: PropTypes.func.isRequired,
+};
+
+AnswerView.defaultProps = {
+  selectedAnswerId: '',
 };
 
 export default AnswerView;
--- src/components/exam-solver-question-view/QuestionView.jsx
+++ src/components/exam-solver-question-view/QuestionView.jsx
@@ -1,16 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+
 import AnswerView from '../exam-solver-answer-view';
 
 const QuestionView = ({
   question,
-}) => (
-  <div>
-    <p>{question.text}</p>
 
-    <AnswerView answers={question.answers} />
-  </div>
+  selectAnswer,
+}) => (
+  <Grid
+    container
+    direction="column"
+    spacing={6}
+  >
+    <Grid item>
+      <div className="question-text">
+        <Typography variant="h5">{question.text}</Typography>
+      </div>
+    </Grid>
+
+    <Divider />
+
+    <Grid item>
+      <AnswerView
+        answers={question.answers}
+        selectedAnswerId={question.selectedAnswerId}
+        onAnswerChange={(e) => {
+          const { value } = e.target;
+          const answerId = value;
+
+          selectAnswer(question.id, answerId);
+        }}
+      />
+    </Grid>
+  </Grid>
 );
 
 QuestionView.propTypes = {
@@ -18,7 +45,9 @@ QuestionView.propTypes = {
     id: PropTypes.string,
     text: PropTypes.string,
     answers: PropTypes.arrayOf(PropTypes.object),
+    selectedAnswerId: PropTypes.string,
   }).isRequired,
+  selectAnswer: PropTypes.func.isRequired,
 };
 
 export default QuestionView;
