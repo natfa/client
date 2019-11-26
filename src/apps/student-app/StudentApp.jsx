366e8a4d671d97fd247e992b4655767c3b39fef1
Nikola Anastasov Velichkov <federlizer@gmail.com>
2019-11-26 11:31:14 +0100

    layout student app design

--- src/apps/student-app/StudentApp.jsx
+++ src/apps/student-app/StudentApp.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 import withLayout from '../../utils/withLayout';
 
+import ExamList from '../../containers/exam-list';
+import StudentExamView from '../../containers/student-exam-view';
+import ExamSolver from '../../containers/exam-solver';
+
 const StudentApp = () => (
-  <p>Student app :)</p>
+  <Switch>
+    <Route path="/exam/:id">
+      <StudentExamView />
+    </Route>
+
+    <Route path="/solve/:id">
+      <ExamSolver />
+    </Route>
+
+    <Route path="/">
+      <ExamList />
+    </Route>
+  </Switch>
 );
 
 export default withLayout(StudentApp);
--- src/pages/student.jsx
+++ src/pages/student.jsx
@@ -1,6 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import LoadingAnimation from '../components/loading-animation';
 import StudentApp from '../apps/student-app';
 
-ReactDOM.render(<StudentApp />, document.getElementById('root'));
+import { getActiveSession } from '../api/session';
+import withRouter from '../utils/withRouter';
+
+(async function IIFE() {
+  const rootNode = document.getElementById('root');
+
+  ReactDOM.render(<LoadingAnimation />, rootNode);
+
+  let authenticated = null;
+  try {
+    authenticated = await getActiveSession();
+  } catch (err) {
+    console.error(err);
+  }
+
+  if (!authenticated) {
+    window.location.pathname = '/landing';
+    return;
+  }
+
+  const StudentAppWithRouter = withRouter(StudentApp, '/student');
+
+  ReactDOM.render(<StudentAppWithRouter />, rootNode);
+}());
