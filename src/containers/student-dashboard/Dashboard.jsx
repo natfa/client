c226af556b27a723eeec76204a107c4c12062144
Nikola Anastasov Velichkov <federlizer@gmail.com>
2019-12-20 15:09:45 +0100

    implement a student dashboard

---
+++ src/containers/student-dashboard/Dashboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Dashboard() {
+  return (
+    <p>This is the student&aposs dashboard</p>
+  );
+}
+
+export default Dashboard;
---
+++ src/containers/student-dashboard/index.js
@@ -0,0 +1,3 @@
+import StudentDashboard from './Dashboard';
+
+export default StudentDashboard;
