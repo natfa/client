8d83aa92e033d29bde03ee256fd3f0360f93f125
Nikola Anastasov Velichkov <federlizer@gmail.com>
2019-12-28 13:09:39 +0100

    implement pagination

--- src/containers/question-list/QuestionList.jsx
+++ src/containers/question-list/QuestionList.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
+import Grid from '@material-ui/core/Grid';
+import Divider from '@material-ui/core/Divider';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import BackIcon from '@material-ui/icons/ArrowBack';
+import ForwardIcon from '@material-ui/icons/ArrowForward';
+
 import QuestionListItem from '../../components/question-list-item';
-import questionAPI from '../../api/question';
+import LoadingAnimation from '../../components/loading-animation';
+
+import questionApi from '../../api/question';
 
 import './styles.css';
 
@@ -10,23 +21,28 @@ class QuestionList extends React.Component {
     super(props);
 
     this.state = {
+      searchValue: '',
       questionId: undefined,
-      questions: [],
+      questions: null,
+      page: 1,
     };
 
     this.handleOpen = this.handleOpen.bind(this);
     this.handleClose = this.handleClose.bind(this);
     this.handleUpdate = this.handleUpdate.bind(this);
     this.handleDelete = this.handleDelete.bind(this);
+
+    this.handleSearchChange = this.handleSearchChange.bind(this);
+    this.handlePageChange = this.handlePageChange.bind(this);
   }
 
-  async componentDidMount() {
-    try {
-      const questions = await questionAPI.getAll();
-      this.setState((state) => ({ ...state, questions }));
-    } catch (err) {
-      console.error(err);
-    }
+  componentDidMount() {
+    questionApi
+      .getAll()
+      .then((questions) => {
+        this.setState((state) => ({ ...state, questions }));
+      })
+      .catch((err) => console.error(err));
   }
 
   handleOpen(id) {
@@ -38,7 +54,7 @@ class QuestionList extends React.Component {
   }
 
   handleUpdate(data) {
-    questionAPI
+    questionApi
       .updateOne(data)
       .then((response) => {
         if (!response.success) {
@@ -67,30 +83,132 @@ class QuestionList extends React.Component {
   }
 
   handleDelete(id) {
-    questionAPI.deleteOneById(id);
+    questionApi.deleteOneById(id);
 
     this.setState((state) => ({
       questions: state.questions.filter((q) => q.id !== id),
     }));
   }
 
+  handleSearchChange(e) {
+    const { value } = e.target;
+
+    this.setState((state) => ({ ...state, searchValue: value }));
+  }
+
+  handlePageChange(newPage) {
+    this.setState((state) => ({ ...state, page: newPage }));
+
+    // scroll to top automatically
+    // the timeout is so that we can "make sure" that the page scrolls
+    // since the setState function will execute sometime in the near future
+    setTimeout(() => {
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth',
+      });
+    }, 100);
+  }
+
   render() {
-    const { questionId, questions } = this.state;
+    const {
+      questionId,
+      questions,
+      searchValue,
+      page,
+    } = this.state;
+
+    if (questions === null) {
+      return <LoadingAnimation />;
+    }
+
+    // round up so that all questions can be housed somewhere
+    const pages = Math.ceil(questions.length / 10);
 
     return (
-      <div className="question-list">
-        {questions.map((question) => (
-          <QuestionListItem
-            key={question.id}
-            open={question.id === questionId}
-            question={question}
-            onOpen={() => this.handleOpen(question.id)}
-            onClose={this.handleClose}
-            onUpdate={this.handleUpdate}
-            onDelete={() => this.handleDelete(question.id)}
-          />
-        ))}
-      </div>
+      <Grid
+        container
+        direction="column"
+        spacing={2}
+      >
+        {questions.map((question, i) => {
+          const minIndex = page * 10 - 10;
+          const maxIndex = page * 10 - 1;
+
+          // skip rendering if not within own page
+          if (i < minIndex || i > maxIndex) return null;
+
+          return (
+            <Grid
+              key={question.id}
+              item
+            >
+              <QuestionListItem
+                key={question.id}
+                open={question.id === questionId}
+                question={question}
+                onOpen={() => this.handleOpen(question.id)}
+                onClose={this.handleClose}
+                onUpdate={this.handleUpdate}
+                onDelete={() => this.handleDelete(question.id)}
+              />
+            </Grid>
+          );
+        })}
+
+        <Grid
+          item
+          container
+          direction="row"
+          wrap="nowrap"
+          justify="space-between"
+          alignItems="center"
+        >
+          <Grid item>
+            <IconButton
+              disabled={page <= 1}
+              onClick={() => this.handlePageChange(page - 1)}
+            >
+              <BackIcon />
+            </IconButton>
+          </Grid>
+
+          <Grid
+            item
+            container
+            direction="row"
+            wrap="nowrap"
+            justify="center"
+          >
+            {[...Array(pages).keys()].map((_, i) => (
+              <Grid
+                key={i} // eslint-disable-line react/no-array-index-key
+                item
+              >
+                <Button
+                  size="large"
+                  onClick={() => this.handlePageChange(i + 1)}
+                  color={page === i + 1 ? 'primary' : 'default'}
+                  variant={page === i + 1 ? 'outlined' : 'text'}
+                >
+                  {i + 1}
+                </Button>
+              </Grid>
+            ))}
+          </Grid>
+
+          <Grid item>
+            <IconButton
+              disabled={page === pages}
+              onClick={() => this.handlePageChange(page + 1)}
+            >
+              <ForwardIcon />
+            </IconButton>
+          </Grid>
+        </Grid>
+
+      </Grid>
     );
   }
 }
